dda0fe268a68be9a99835b40122b258acb430462
Anand kumar <anand_k@cs.iitr.ac.in>
2021-07-23 04:10:07 +0530

    feat: add local_storage service

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,9 +1,12 @@
 import 'package:get_it/get_it.dart';
+import 'package:task_dot_do/services/local_storage_service.dart';
 import 'package:task_dot_do/viewmodels/home_viewmodel.dart';
 
 GetIt locator = GetIt.instance;
 
 Future<void> setUpLocator() async {
   locator.registerFactory<HomeViewmodel>(() => HomeViewmodel());
+  var localStorage = await LocalStorageService.getInstance();
+  locator.registerSingleton<LocalStorageService>(localStorage);
   return;
 }
---
+++ lib/services/local_storage_service.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalStorageService {
+  static late LocalStorageService _instance;
+  static late SharedPreferences _preferences;
+
+  static const String authStateKey = 'is_LoggedIn';
+  static const String firstLoginKey = 'is_first_login';
+
+  static Future<LocalStorageService> getInstance() async {
+    _instance = LocalStorageService();
+    _preferences = await SharedPreferences.getInstance();
+    return _instance;
+  }
+
+  dynamic _getFromDisk(String key) {
+    var value = _preferences.get(key);
+    return value;
+  }
+
+  void _saveToDisk<T>(String key, T value) {
+    if (value is bool)
+      _preferences.setBool(key, value);
+    else if (value is double)
+      _preferences.setDouble(key, value);
+    else if (value is int)
+      _preferences.setInt(key, value);
+    else if (value is String)
+      _preferences.setString(key, value);
+    else if (value is List<String>)
+      _preferences.setStringList(key, value);
+    else
+      debugPrint('passed value is none of the specified type');
+  }
+
+  bool get isLoggedIn => _getFromDisk(authStateKey) ?? false;
+  set isLoggedIn(bool isLoggedIn) => _saveToDisk(authStateKey, isLoggedIn);
+
+  bool get isFirstLogin => _getFromDisk(firstLoginKey) ?? false;
+  set isFirstLogin(bool isFirstLogin) =>
+      _saveToDisk(firstLoginKey, isFirstLogin);
+}
