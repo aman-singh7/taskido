73c92fb21a2ea3740b1440ca9c720430525a5cdf
aman-singh7 <aman2@me.iitr.ac.in>
2021-07-30 13:28:00 +0530

    changes in group bottom sheet

--- lib/ui/components/fab_body/group_function_bottom_sheet.dart
+++ lib/ui/components/fab_body/group_function_bottom_sheet.dart
@@ -50,10 +50,10 @@ class GroupFunctionBottomSheet extends StatelessWidget {
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
                 border: Border.all(color: Colors.black),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/$iconName'),
-                  fit: BoxFit.contain,
-                ),
+              ),
+              child: Image.asset(
+                'assets/images/$iconName',
+                fit: BoxFit.contain,
               ),
             ),
             Text(title),
